a7400f3e53f33c364ff3baae7829584d456976a0
Andrea Sierra <andrea.sierra05@uptc.edu.co>
2024-05-23 20:44:57 -0500

    Add Final Reports Console

--- ManagerTournament.py
+++ ManagerTournament.py
@@ -66,36 +66,25 @@ class ManagerTournament:
     
   # Método para simular la anotación de un punto
     def get_winner_points(self, player_service: Player, player_subtract: Player) -> Player:
-        print("get_winner_points")
         score = self.launch()
-        #print("score",score)
         if score == 1:
             self.add_player_points(player_service)
         else:
             if (self.ball_return() == 0):
-                #print("Regular1111111111")
                 score1 = self.launch()
-               # print("score", score1)
                 if score1 == 0:
-                    #print("Hola")
                     if (self.ball_return()==0):
-                        #print("Regular")
                         self.get_winner_points(player_service,player_subtract)
                     elif(self.ball_return()==1):
-                        #print("Buena")
                         self.add_player_points(player_subtract)
                     else:
-                        #print("Error")
                         self.add_player_points(player_service)
                 else:
                     self.add_player_points(player_subtract)
             elif(self.ball_return() == 1):
-                #print("Buena111111111111")
                 self.add_player_points(player_service)
             else:
-                #print("Error1111111111")
                 self.add_player_points(player_subtract)
-            #Retorna el jugador que anotó punto
         return player1 if player1.get_points() > player2.get_points() else player2
  
     #Metodo para Simular un juego
@@ -109,30 +98,21 @@ class ManagerTournament:
         isPlaying = True
 
         while isPlaying:
-            print("whileeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
             if(self.is_successful_service(player_service) == True):
                 self.get_winner_points(player_service,player_subtractor)
             else:
-                #print("Falló")
                 player_service.set_service_failure(player_service.get_service_failure()+1)
 
                 if(self.double_failure_service(player_service) == True):
-                    #print("Falló dos veces")
                     self.subtract_points(player_service, player_subtractor)
 
             if player_service.get_points() == 4 or player_subtractor.get_points() == 4:
                 if abs(player_service.get_points() - player_subtractor.get_points()) == 2:
-                    # Juego terminado
-                    print("¡Jugador ganador!")
                     self.establish_winner_game(player1, player2)
                     break
                 elif player_service.get_points() == 4 and player_subtractor.get_points() == 4:
-                    # Empate
-                    #print("¡Empate!---------------------------------------------------------------")
                     self.get_winner_game(player_service, player_subtractor)
                     if abs(player_service.get_points() - player_subtractor.get_points()) == 2:
-                    # Juego terminado
-                        print("¡Jugador ganador!")
                         self.establish_winner_game(player1, player2)
                         isPlaying = False
 
@@ -140,7 +120,7 @@ class ManagerTournament:
                 if abs(player_service.get_points() - player_subtractor.get_points()) >= 2:
                     self.establish_winner_game(player1, player2)
                     isPlaying = False
-        player_service, player_subtractor = player_subtractor, player_service    
+        #player_service, player_subtractor = player_subtractor, player_service    
         return player1 if player1.get_points() > player2.get_points() else player2
         
     #Metodo para añadir puntos a unjugador
@@ -226,13 +206,9 @@ class ManagerTournament:
         if player1.get_points() > player2.get_points():
             player1.set_games_won(player1.get_games_won()+1)
             player1.set_total_games_won(player1.get_total_games_won()+1)
-            #print("p1", player1.get_games_won())
-            #print("p1 poitssssssssssssssssssssssss", player1.get_points())
         elif player2.get_points() > player1.get_points():
             player2.set_games_won(player2.get_games_won()+1)
             player2.set_total_games_won(player2.get_total_games_won()+1)
-           # print("p2",player2.get_games_won())
-           # print("p2 poitssssssssssssssssssssssss", player2.get_points())
 
 
     #Metodo para definir el ganador de un set
@@ -242,7 +218,6 @@ class ManagerTournament:
         isPlaying = True
 
         while isPlaying:
-            print("playing set")
             self.create_game
             self.start_game(player1, player2)
 
@@ -251,7 +226,6 @@ class ManagerTournament:
                     self.establish_winner_Set(player1, player2)
                     isPlaying = False
             if( player1.get_games_won() == 6 and player2.get_games_won() == 6):
-               # print("Empate|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
                 self.tie_break(player1, player2)
                 self.establish_winner_Set(player1, player2)
                 isPlaying = False
@@ -262,11 +236,9 @@ class ManagerTournament:
         if player1.get_games_won() > player2.get_games_won():
             player1.set_sets_won(player1.get_sets_won()+1)
             player1.set_total_sets_won(player1.get_total_sets_won()+1)
-            #print("p1", player1.get_sets_won())
         elif player2.get_games_won() > player1.get_games_won():
             player2.set_sets_won(player2.get_sets_won()+1)
             player2.set_total_sets_won(player2.get_total_sets_won()+1)
-           # print("p2", player2.get_sets_won())
 
     #Metodo para Establecer el ganador de un Set en caso de empate
     def tie_break(self, player1: 'Player', player2: 'Player') -> 'Player':
@@ -275,21 +247,16 @@ class ManagerTournament:
         isPlaying = True
 
         while isPlaying:
-            print("Tiebreakkkkkkkkkkkkkkkkkkkkkkkkk")
             if(self.is_successful_service(player1) == True):
                 self.get_winner_points(player1,player2)
             else:
-                #print("Falló")
                 player1.set_service_failure(player1.get_service_failure()+1)
 
                 if(self.double_failure_service(player1) == True):
-                    #print("Falló dos veces")
                     self.subtract_points(player1, player2)
 
             if player1.get_points() == 7 or player2.get_points() == 7:
                 if abs(player1.get_points() - player2.get_points()) == 2:
-                    # Juego terminado
-                    print("¡Jugador ganador!")
                     self.establish_winner_game(player1, player2)
                     isPlaying = False
 
@@ -297,7 +264,7 @@ class ManagerTournament:
                 if abs(player1.get_points() - player2.get_points()) >= 2:
                     self.establish_winner_game(player1, player2)
                     isPlaying = False
-        player1, player2 = player2, player1
+        #player1, player2 = player2, player1
             
         return player1 if player1.get_points() > player2.get_points() else player2
         
@@ -313,38 +280,32 @@ class ManagerTournament:
         isPlaying = True
         self.assign_tournament_type(tournament)
         while isPlaying:
-            print("Playing Torneo")
             
             if tournament.get_tournament_type() == "Best of 5":
                 self.define_set_winner(player1, player2)
                 if player1.get_sets_won() == 3 or player2.get_sets_won() == 3:
-                    print("Ganador Torneo Best of 5")
                     self.establish_winner_tournament(player1, player2, tournament)
                     isPlaying = False
 
             elif tournament.get_tournament_type() == "Best of 3":
                 self.define_set_winner(player1, player2)
                 if player1.get_sets_won() == 2 or player2.get_sets_won() == 2:
-                    print("Ganador Torneo Best of 3")
                     self.establish_winner_tournament(player1, player2, tournament)
                     isPlaying = False
             
-        #self.final_tournament(player1,player2)
         return player1 if player1.get_sets_won() > player2.get_sets_won() else player2
 
     #Metodo para Establecer el ganador de un Torneo
     def establish_winner_tournament(self, player1: 'Player', player2: 'Player', tournament: 'Tournament')-> None:
         if player1.get_sets_won() > player2.get_sets_won():
             player1.set_tournaments_won(player1.get_tournaments_won()+1)
-            print("SUMA TORNEO +1 j1")
-            print(player1.get_total_tournaments_won())
+
             player1.set_total_tournaments_won(player1.get_total_tournaments_won()+1)
             player1.set_type_of_tournament_won(tournament.get_tournament_type())
             
         elif player2.get_sets_won() > player1.get_sets_won():
             player2.set_tournaments_won(player2.get_tournaments_won()+1)
-            print("SUMA TORNEO +1 j2")
-            print(player2.get_total_tournaments_won())
+ 
             player2.set_total_tournaments_won(player2.get_total_tournaments_won()+1)
             player2.set_type_of_tournament_won(tournament.get_tournament_type())
 
@@ -368,6 +329,8 @@ class ManagerTournament:
         player1.set_serves_served(0)
         player2.set_serves_served(0)
 
+        player1.set_tournaments_won(0)
+        player2.set_tournaments_won(0)
 
     #Metodo para Jugar varios torneos
     def playRounds(self, player1: 'Player', player2: 'Player',tournament: 'Tournament'):
@@ -379,38 +342,38 @@ class ManagerTournament:
     #Metodo para Encontrar al jugador con más juagos ganados en cada torneo
     def get_player_more_victories_game(self, player1: 'Player', player2: 'Player') -> 'Player':
         if player1.get_total_games_won() > player2.get_total_games_won():
-            print("Jugador con más juegos ganados", player1.get_total_games_won())
             return player1
         else:
-            print("Jugador con mas juegos ganados", player2.get_total_games_won())
             return player2
 
     #Metodo para Encontrar al jugador con más sets ganados
     def get_player_more_victories_set(self, player1: 'Player', player2: 'Player') -> 'Player':
         if player1.get_total_sets_won() > player2.get_total_sets_won():
-            print("Jugador con más sets ganados", player1.get_total_sets_won())
             return player1
         else:
-            print("Jugador con mas sets ganados", player2.get_total_sets_won())
             return player2
     #Metodo para Encontrar al jugador con más Torneos ganados
     def get_player_more_victories_tournament(self, player1: 'Player', player2: 'Player') -> 'Player':
         if player1.get_total_tournaments_won() > player2.get_total_tournaments_won():
-            print("Jugador con más sets ganados", player1.get_total_tournaments_won())
             return player1
         else:
-            print("Jugador con mas sets ganados", player2.get_total_tournaments_won())
             return player2
     #Metodo para Obtener los puntajes de los jugadores en cada unos de los juegos
-    #Metodo para Obtener al jugador que ganó cada sorteo
-    #Metodo para Obtener al jugador que ganó cada torneo con su puntaje
-
+    def get_player_winner_and_loser_game(self, player1: 'Player', player2: 'Player'):
+        if player1.get_points() > player2.get_points():
+            winner = player1.get_points()
+            loser = player2.get_points()
+            return winner, loser
+        else:
+            winner = player2.get_points()
+            loser = player1.get_points()
+            return winner, loser
 
     #Metodo para obtener los resultados de cada torneo
     def simulacion(self, player1: 'Player', player2: 'Player',tournament: 'Tournament'):
         resultados = []
 
-        for _ in range(5):
+        for _ in range(20):
             print("holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
             resultados.append(self.simulacion_round(player1, player2, tournament))
         
@@ -425,14 +388,15 @@ class ManagerTournament:
         player_with_most_games_won: Player = self.get_player_more_victories_game(player1, player2)
         player_with_most_sets_won: Player = self.get_player_more_victories_set(player1, player2)
         player_with_most_tournaments_won: Player = self.get_player_more_victories_tournament(player1, player2)
+        player_winner_game, player_loser_game = self.get_player_winner_and_loser_game(player1, player2)
 
         resultGame = ResultGame(
             tournament_winner_with_score,
             player_with_most_games_won,
             player_with_most_sets_won,
-            player_with_most_tournaments_won
-            #scores_per_game
-            #initial_raffle_winner_per_game
+            player_with_most_tournaments_won,
+            player_winner_game,
+            player_loser_game
         )
 
         # Imprimir datos del jugador 1
@@ -472,17 +436,17 @@ class ManagerTournament:
 manager = ManagerTournament()
 tournament = manager.create_tournament()
 player1, player2 = manager.create_players()
-print("------------------------------")
+print("------------------------------------------------------------------------")
 #print("Fin Torneo",manager.define_tournament_winner(player1, player2, tournament))
 #print("Fin Torneo",manager.playRounds(player1, player2, tournament))
 
 print("Fin Torneo")
 data = manager.simulacion(player1, player2, tournament)
-#for i in range(len(data)):
-#    print("hola")
-#    data[i].print_results()
+for i in range(len(data)):
+    print("Resultado final torneoooooooooooooooooooooooooooooooooo")
+    data[i].print_results()
 
-print("-----------------------------")
+print("--------------------------------------------------------------------------")
 
 
 """# Crear un set y un juego de muestra
--- ResultGame.py
+++ ResultGame.py
@@ -7,23 +7,22 @@ class ResultGame:
             tournament_winner_with_score: Player, 
             player_with_most_games_won: 'Player',
             player_with_most_sets_won: 'Player',
-            player_with_most_tournaments_won:'Player'
-            
-            
-            #scores_per_game: dict = None,
-            #initial_raffle_winner_per_game: dict = None
+            player_with_most_tournaments_won:'Player',
+            player_winner_game: 'Player',
+            player_loser_game: 'Player'
             ):
         
         self._tournament_winner_with_score = tournament_winner_with_score
         self._player_with_most_games_won = player_with_most_games_won
-        self._player_with_most_sets_won = player_with_most_sets_won #player_with_most_sets_won
-        self._player_with_most_tournaments_won = player_with_most_tournaments_won #player_with_most_tournaments_won
-        
-        self._tournament_winner_with_score = None #tournament_winner_with_score if tournament_winner_with_score is not None else {}
-        self._scores_per_game = None #scores_per_game if scores_per_game is not None else {}
-        self._initial_raffle_winner_per_game = None #initial_raffle_winner_per_game if initial_raffle_winner_per_game is not None else {}
+        self._player_with_most_sets_won = player_with_most_sets_won 
+        self._player_with_most_tournaments_won = player_with_most_tournaments_won 
+        self._player_winne_game = player_winner_game
+        self._player_loser_game = player_loser_game
 
     # Getters
+    def get_tournament_winner_with_score(self) -> 'Player':
+        return self._tournament_winner_with_score
+
     def get_player_with_most_games_won(self) -> 'Player':
         return self._player_with_most_games_won
 
@@ -33,63 +32,41 @@ class ResultGame:
     def get_player_with_most_tournaments_won(self) -> 'Player':
         return self._player_with_most_tournaments_won
 
-    def get_scores_per_game(self) -> dict:
-        return self._scores_per_game
+    def get_player_winner_game(self) -> 'Player':
+        return self._player_winner_game
 
-    def get_initial_raffle_winner_per_game(self) -> dict:
-        return self._initial_raffle_winner_per_game
-
-    def get_tournament_winner_with_score(self) -> dict:
-        return self._tournament_winner_with_score
+    def get_player_loser_game(self) -> 'Player':
+        return self._player_loser_game
 
     # Setters
-    def set_player_with_most_games_won(self, player: 'Player'):
-        self._player_with_most_games_won = player
+    def set_tournament_winner_with_score(self, tournament_winner_with_score: 'Player'):
+        self._tournament_winner_with_score = tournament_winner_with_score
 
-    def set_player_with_most_sets_won(self, player: 'Player'):
-        self._player_with_most_sets_won = player
+    def set_player_with_most_games_won(self, player_with_most_games_won: 'Player'):
+        self._player_with_most_games_won = player_with_most_games_won
 
-    def set_player_with_most_tournaments_won(self, player: 'Player'):
-        self._player_with_most_tournaments_won = player
+    def set_player_with_most_sets_won(self, player_with_most_sets_won: 'Player'):
+        self._player_with_most_sets_won = player_with_most_sets_won
 
-    def set_scores_per_game(self, game_id: str, score: str):
-        self._scores_per_game[game_id] = score
+    def set_player_with_most_tournaments_won(self, player_with_most_tournaments_won: 'Player'):
+        self._player_with_most_tournaments_won = player_with_most_tournaments_won
 
-    def set_initial_raffle_winner_per_game(self, game_id: str, player: 'Player'):
-        self._initial_raffle_winner_per_game[game_id] = player
+    def set_player_winner_game(self, player_winner_game: 'Player'):
+        self._player_winner_game = player_winner_game
 
-    def set_tournament_winner_with_score(self, tournament_id: str, player: 'Player', score: str):
-        self._tournament_winner_with_score[tournament_id] = (player, score)
-
+    def set_player_loser_game(self, player_loser_game: 'Player'):
+        self._player_loser_game = player_loser_game
+        
     # Método para imprimir los resultados en forma de tabla
     def print_results(self):
         print("+------------------------------------------+")
         print("|              Resultados del Juego        |")
         print("+------------------------------------------+")
-        print("| {:<36} | {:<10} |".format("Ganador de cada Torneo", str(self._player_with_most_games_won.print_player_data())))
+        print("| {:<36} | {:<10} |".format("Ganador de cada Torneo", str(self._player_with_most_games_won.get_player_id())))
         print("| {:<36} | {:<10} |".format("Jugador con más juegos ganados", str(self._player_with_most_games_won.get_total_games_won())))
         print("| {:<36} | {:<10} |".format("Jugador con más sets ganados", str(self._player_with_most_sets_won.get_total_sets_won())))
         print("| {:<36} | {:<10} |".format("Jugador con más torneos ganados", str(self._player_with_most_tournaments_won.get_total_tournaments_won())))
+        print("| {:<36} | {:<10} |".format("Jugador ganador", str(self._player_winne_game)))
+        print("| {:<36} | {:<10} |".format("Jugador perdedor", str(self._player_loser_game)))
         print("+------------------------------------------+")
-
-        """print("\n+------------------------------------------+")
-        print("|        Puntaje de los jugadores por juego        |")
-        print("+------------------------------------------+")
-        for game_id, score in self._scores_per_game:
-            print("| {:<36} | {:<10} |".format(game_id, score))
-        print("+------------------------------------------+")"""
-
-        """print("\n+------------------------------------------+")
-        print("|  Jugador que ganó el sorteo inicial por juego |")
-        print("+------------------------------------------+")
-        for game_id, player in self._initial_raffle_winner_per_game:
-            print("| {:<36} | {:<10} |".format(game_id, str(player)))
-        print("+------------------------------------------+")"""
-
-        """print("\n+------------------------------------------+")
-        print("|         Ganador de cada torneo y puntaje         |")
-        print("+------------------------------------------+")
-        for tournament_id, player, score in self._tournament_winner_with_score:
-            print("| {:<20} | {:<10} | {:<5} |".format(tournament_id, str(player), score))
-        print("+------------------------------------------+")"""
 
